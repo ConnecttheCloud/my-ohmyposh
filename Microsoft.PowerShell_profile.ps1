844ce774dfe5f181e3882e28a07bf433cdc8ca0e
AddictedtoCloud <92588430+ConnecttheCloud@users.noreply.github.com>
2023-09-19 23:18:36 +0800

    Initial added

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\aliens.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\pixelrobots.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\blue-owl.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\mytheme.omp.json" |  Invoke-Expression
+
+
+
+
+new-alias grep findstr
+#FFEB3B
+
+
+
+##ADD below line to configure for WSL Linux
+
+#window_user="/mnt/c/users/shwe.linhtet.OCLMMLTAS54"
+#eval "$(oh-my-posh --init --shell bash --config $window_user/AppData/Local/Programs/oh-my-posh/themes/mytheme.omp.json)"
