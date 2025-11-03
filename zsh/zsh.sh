2f1b429811f5770fc5522f7346a4988ad922dd1f
AddictedtoCloud <92588430+ConnecttheCloud@users.noreply.github.com>
2025-11-03 13:59:12 +0800

    Disable .zshrc backup in zsh.sh
    Comment out backup logic for .zshrc file.

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,13 +1,11 @@
 curl -fSsL https://raw.githubusercontent.com/ConnecttheCloud/my-ohmyposh/refs/heads/main/zsh/install.sh | bash 
 
-`zsh`
-
 # Backup .zshrc file if exist
-ZSHRC="$HOME/.zshrc"
-if [ -f "$ZSHRC" ]; then
-echo "Backing up existing .zshrc to .zshrc.backup"
-mv "$ZSHRC" "$ZSHRC.backup"
-fi
+# ZSHRC="$HOME/.zshrc"
+# if [ -f "$ZSHRC" ]; then
+# echo "Backing up existing .zshrc to .zshrc.backup"
+# mv "$ZSHRC" "$ZSHRC.backup"
+# fi
 
 curl -o ~/.zshrc https://raw.githubusercontent.com/ConnecttheCloud/my-ohmyposh/refs/heads/main/zsh/.zshrc
 
